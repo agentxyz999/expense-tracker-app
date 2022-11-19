df53fd8fe351330744b18968df32b042a9c6bf4e
Rolie Conde <onefivezero999@gmail.com>
2022-11-19 14:05:35 +0800

    display the data from App.js using map function

--- src/App.js
+++ src/App.js
@@ -2,9 +2,29 @@ import React from 'react';
 import ExpenseItem from './components/ExpenseItem';
 
 const App = () => {
+  const expenses = [
+    { 
+      id: 1,
+      item: 'HDD',
+      amount: 4000,
+      date: new Date(2022, 3, 21),
+    },
+    {
+      id: 2,
+      item: 'Monitor',
+      amount: 6000,
+      date: new Date(2022, 11, 16),
+    },
+    {
+      id: 3,
+      item: 'Keyboard',
+      amount: 1000,
+      date: new Date(2022, 10, 6),
+    }
+  ];
   return (
     <div className='container'>
-      <ExpenseItem />
+      <ExpenseItem expenses={ expenses }/>
     </div>
   )
 }
--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,23 +1,33 @@
-import React from 'react';
-import './ExpenseItem.css';
-import { Card, Row, Col } from 'react-bootstrap';
-import CardHeader from 'react-bootstrap/esm/CardHeader';
+import React from "react";
+import "./ExpenseItem.css";
+import { Card, Row, Col } from "react-bootstrap";
 
-const ExpenseItem = () => {
+const ExpenseItem = ({ expenses }) => {
+  console.log(expenses);
   return (
-    <Card>
-        <CardHeader className='text-center bg-dark rounded'>
-            <h1>My Expense Tracker App</h1>
-        </CardHeader>
-        <Card.Body className='text-center rounded'>
-            <Row>
-                <Col>November 1, 2022</Col>
-                <Col>Monitor</Col>
-                <Col className='expense__price'>Php 4000</Col>
+    <Card className="shadow rounded">
+      <Card.Header className="text-center card__header">
+        <h1>My Expense Tracker App</h1>
+      </Card.Header>
+      <Card.Body className="text-center rounded-bottom card__body">
+        { expenses.map((expense) => {
+          return (
+            <Row key={ expense.id } className='shadow-lg m-2 p-2 rounded align-items-center'>
+              <Col className="expense__date">
+                <div className="date__card">
+                  <div className="month">{ expense.date.toLocaleString('en-US', { month: 'long' }) }</div>
+                  <div className="year">{ expense.date.getFullYear() }</div>
+                  <div className="day">{ expense.date.toLocaleString('en-US', { day: '2-digit' }) }</div>
+                </div>
+              </Col>
+              <Col className="expense__item"> { expense.item } </Col>
+              <Col className="expense__amount"> Php { expense.amount } </Col>
             </Row>
-        </Card.Body>
+          );
+        }) }
+      </Card.Body>
     </Card>
-  )
-}
+  );
+};
 
 export default ExpenseItem;
