ce31a6f79678dd4f16ce2e60e8eb1de547ff0c9b
Rolie Conde <onefivezero999@gmail.com>
2023-01-11 03:20:55 +0800

    Add chartJS again

---
+++ src/components/Chart/Chart.jsx
@@ -0,0 +1,90 @@
+import React from "react";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+const Chart = () => {
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top",
+      },
+      title: {
+        display: true,
+        text: "Expenses by Year",
+      },
+    },
+  };
+
+  const labels = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: "Amount in Peso",
+        data: [332, 232, 231, 412, 223, 114, 242, 564, 343, 231, 76, 200],
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.3)",
+          "rgba(54, 162, 235, 0.3)",
+          "rgba(255, 206, 86, 0.3)",
+          "rgba(25, 99, 132, 0.3)",
+          "rgba(54, 162, 235, 0.3)",
+          "rgba(255, 206, 86, 0.3)",
+          "rgba(105, 99, 132, 0.3)",
+          "rgba(54, 162, 235, 0.3)",
+          "rgba(255, 206, 86, 0.3)",
+          "rgba(212, 99, 132, 0.3)",
+          "rgba(54, 162, 235, 0.3)",
+          "rgba(255, 206, 86, 0.3)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+        ],
+      },
+    ],
+  };
+  return <Bar options={options} data={data} />;
+};
+
+export default Chart;
